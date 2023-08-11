8a7bc9a5cc8cdce8dbb7b348f56a068bf5eadceb
Aluno <aluno@etec.sp.gov.br>
2023-08-11 13:49:09 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
-  return (
+const App = () =>{
+  const [inputValues, setInputValues ] = useState({
+    input1:'',
+
+  });
+
+  const[soma, setSoma] = useState(0);
+
+  const handleInputChange = (inputName, value) =>{
+    setInputValues({...inputValues, [inputName]:value});
+  };
+  
+  const handleSoma = () => {
+const { input1 } = inputValues;
+
+const result = parseInt(input1);
+
+setSoma(result);
+  };
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.label}>Primeiro Número:</Text>
+    <TextInput
+    style={styles.input}
+    onChangeText={(value) => handleInputChange('input1', value)}
+    value={inputValues.input1}
+    keyboardType='numeric'
+    />
+
+    <Button title='Somar' onPress={handleSoma}/>
+    <Text>Resultado: {soma}</Text>
     </View>
   );
-}
+
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#E0F2F1',
+    padding: 20, 
+  }
 });
+
+export default App;
